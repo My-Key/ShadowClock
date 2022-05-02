a02e7feef5e7444e2b6781fce6c049b112d634e6
Maciej <0my0key0@gmail.com>
2022-05-02 20:49:04 +0200

    Update textures Cleanup

--- src/BowlWatchy.cpp
+++ src/BowlWatchy.cpp
@@ -1,19 +1,19 @@
 #include "BowlWatchy.h"
 
-const float VOLTAGE_MIN = 3.4;
-const float VOLTAGE_MAX = 4.2;
+const float VOLTAGE_MIN = 3.4f;
+const float VOLTAGE_MAX = 4.2f;
 const float VOLTAGE_RANGE = VOLTAGE_MAX - VOLTAGE_MIN;
 
 const float MINUTE_ANGLE = 360 / 60;
 const float HOUR_ANGLE = 360 / 12;
 
 const float CONE_RADIUS = 10;
-const float CONE_HEIGHT = 80;
+const float CONE_HEIGHT = 50;
 
-const Vector3<float> CONE_CENTER = {99.5, 99.5, 0};
-const Vector3<float> NORMAL_DIR = {0, 0, 1};
+const Vector3<float> CONE_CENTER = {99.5f, 99.5f, 0.0f};
+const Vector3<float> NORMAL_DIR = {0.0f, 0.0f, 1.0f};
 const Vector3<float> CONE_TOP = CONE_CENTER + NORMAL_DIR * CONE_HEIGHT;
-const Vector3<float> VIEW_DIR = {0,0,1};
+const Vector3<float> VIEW_DIR = {0.0f, 0.0f, 1.0f};
 
 void BowlWatchy::drawWatchFace()
 {
@@ -40,10 +40,10 @@ int BowlWatchy::getBatteryFill(int steps)
 static float clamp(const float& value)
 {
   float outValue = value;
-  if (outValue > 1.0)
-        outValue = 1.0;
-  else if (outValue < 0.0)
-        outValue = 0.0;
+  if (outValue > 1.0f)
+        outValue = 1.0f;
+  else if (outValue < 0.0f)
+        outValue = 0.0f;
 
   return outValue;
 }
@@ -71,18 +71,18 @@ static bool PointInTriangle (const Vector3<float>& pt, const Vector3<float>& v1,
 static void BlinPhong(const int x, const int y, const Vector3<float>& normal, const Vector3<float>& lightDir, const Vector3<float>& halfView, const float& gloss, 
 const Vector3<float>& coneBase1, const Vector3<float>& coneBase2, const Vector3<float>& coneBase3, float& intensity, float& specularIntensity)
 {
-  float shadow = 1.0;
+  float shadow = 1.0f;
 
-  Vector3<float> point = {x,y,0};
+  Vector3<float> point = {x,y,0.0f};
 
   if ((point - CONE_CENTER).sqrMagnitude() > CONE_RADIUS * CONE_RADIUS && PointInTriangle(point, coneBase1, coneBase2, coneBase3))
-    shadow = 0;
+    shadow = 0.0f;
 
   float NdotL = Vector3<float>::dotProduct(normal, lightDir);
   intensity += clamp(NdotL) * shadow;
 
   float NdotH = Vector3<float>::dotProduct(normal, halfView);
-  specularIntensity += pow(clamp(NdotH), gloss / 8) * shadow;
+  specularIntensity += pow(clamp(NdotH), gloss / 2) * shadow;
 }
 
 void BowlWatchy::drawTime()
@@ -90,18 +90,18 @@ void BowlWatchy::drawTime()
   int hour = currentTime.Hour;
   int minute = currentTime.Minute;
 
-  Vector3<float> minuteLight = {0,-1,1};
+  Vector3<float> minuteLight = {0.0f,-1.0f,0.75f};
   minuteLight = Vector3<float>::rotateVector(minuteLight, MINUTE_ANGLE * minute);
   minuteLight.normalize();
 
   Vector3<float> coneBaseMinute1 = Vector3<float>::rotateVectorByRightAngle(minuteLight, 1);
-  coneBaseMinute1.z = 0;
+  coneBaseMinute1.z = 0.0f;
   coneBaseMinute1.normalize();
   coneBaseMinute1.scale(CONE_RADIUS);
   coneBaseMinute1 = coneBaseMinute1 + CONE_CENTER;
 
   Vector3<float> coneBaseMinute2 = Vector3<float>::rotateVectorByRightAngle(minuteLight, 3);
-  coneBaseMinute2.z = 0;
+  coneBaseMinute2.z = 0.0f;
   coneBaseMinute2.normalize();
   coneBaseMinute2.scale(CONE_RADIUS);
   coneBaseMinute2 = coneBaseMinute2 + CONE_CENTER;
@@ -111,18 +111,18 @@ void BowlWatchy::drawTime()
   Vector3<float> minuteHalfView = minuteLight + VIEW_DIR;
   minuteHalfView.normalize();
 
-  Vector3<float> hourLight = {0,-0.5,1};
+  Vector3<float> hourLight = {0.0f,-0.5f,0.6f};
   hourLight = Vector3<float>::rotateVector(hourLight, HOUR_ANGLE * ((hour % 12) + minute/60.0) );
   hourLight.normalize();
 
   Vector3<float> coneBaseHour1 = Vector3<float>::rotateVectorByRightAngle(hourLight, 1);
-  coneBaseHour1.z = 0;
+  coneBaseHour1.z = 0.0f;
   coneBaseHour1.normalize();
   coneBaseHour1.scale(CONE_RADIUS);
   coneBaseHour1 = coneBaseHour1 + CONE_CENTER;
 
   Vector3<float> coneBaseHour2 = Vector3<float>::rotateVectorByRightAngle(hourLight, 3);
-  coneBaseHour2.z = 0;
+  coneBaseHour2.z = 0.0f;
   coneBaseHour2.normalize();
   coneBaseHour2.scale(CONE_RADIUS);
   coneBaseHour2 = coneBaseHour2 + CONE_CENTER;
@@ -140,7 +140,7 @@ void BowlWatchy::drawTime()
     {
       int index = x * 3 + 200 * 3 * y;
       Vector3<float> normal = {Normal[index], Normal[index + 1], Normal[index + 2]};
-      normal = normal * (2 / 255.0);
+      normal = normal * (2 / 255.0f);
       normal = normal - 1;
       normal.x *= -1;
       normal.normalize();
@@ -152,7 +152,7 @@ void BowlWatchy::drawTime()
       BlinPhong(x, y, normal, minuteLight, minuteHalfView, gloss, coneBaseMinute, coneBaseMinute1, coneBaseMinute2, intensity, specularIntensity);
       BlinPhong(x, y, normal, hourLight, hourHalfView, gloss, coneBaseHour, coneBaseHour1, coneBaseHour2, intensity, specularIntensity);
 
-      bool white = (intensity * Diffuse[x + 200 * y] + specularIntensity * gloss) * 0.5 > BlueNoise200[y * 200 + x];
+      bool white = (intensity * Diffuse[x + 200 * y] + specularIntensity * 255) * 0.5f > BlueNoise200[y * 200 + x];
       display.drawPixel(x, y, white ? GxEPD_WHITE : GxEPD_BLACK);
     }
   }
